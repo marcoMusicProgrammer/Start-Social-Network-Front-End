1948e93a258b363da54f2cfc70f03264517bc198
marco <marco.maisto@hotmail.it>
2025-01-20 14:14:07 +0100

    Authenticatin API and Login page

---
+++ src/app/services/request-client.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {UserDTOReq} from '../models/UserDTOReq';
+import {Observable} from 'rxjs';
+import {UserDTOLoginReq} from '../models/UserDTOLoginReq';
+import {UserDTOResp} from '../models/UserDTOResp';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RequestClientService {
+
+  constructor(private http: HttpClient) {}
+
+  getAllUsers(): Observable<UserDTOResp[]> {
+    return this.http.get<UserDTOResp[]>("/api/users")
+  }
+
+  loginUser(toInsert: UserDTOLoginReq): Observable<UserDTOLoginReq> {
+    return this.http.post<UserDTOLoginReq>("/api/authentication/login",toInsert);
+  }
+
+
+}
