b0eab18110870195882d530d7d5d5a81594737df
marco <marco.maisto@hotmail.it>
2025-01-20 14:27:28 +0100

    Login page v2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {LoginPageComponent} from './components/login-page/login-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoginPageComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
