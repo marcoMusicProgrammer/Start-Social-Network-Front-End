fd90f023133647c9a7d6e16c9b6efa41703055eb
jacopo <jacopo.cariato98@gmail.com>
2025-01-21 13:01:59 +0100

    user page: cover image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from './components/login-page/login-page.component';
 import {SigninPageComponent} from './components/signin-page/signin-page.component';
+import {UserProfilePageComponent} from './components/user-profile-page/user-profile-page.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent],
+  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
--- src/app/components/user-profile-page/user-profile-page.component.ts
+++ src/app/components/user-profile-page/user-profile-page.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-user-profile-page',
   imports: [],
   templateUrl: './user-profile-page.component.html',
+  standalone: true,
   styleUrl: './user-profile-page.component.css'
 })
 export class UserProfilePageComponent {
