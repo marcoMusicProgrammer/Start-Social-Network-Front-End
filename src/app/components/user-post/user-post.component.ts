ccb37f0d7e91e51780dedd21fc478587f2e42898
Marco <marco.maisto@hotmail.it>
2025-01-22 00:24:12 +0100

    Profile page and posts

---
+++ src/app/components/user-post/user-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-post',
+  imports: [],
+  templateUrl: './user-post.component.html',
+  styleUrl: './user-post.component.css'
+})
+export class UserPostComponent {
+
+
+}
