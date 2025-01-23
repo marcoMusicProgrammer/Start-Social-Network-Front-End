a1e3546290b2d1b349341f46d77e5f09540aa931
marco <marco.maisto@hotmail.it>
2025-01-23 16:06:12 +0100

    User details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {LoginPageComponent} from './components/login-page/login-page.component';
 import {SigninPageComponent} from './components/signin-page/signin-page.component';
 import {UserProfilePageComponent} from './components/user-profile-page/user-profile-page.component';
 import {RecommendedGamesComponent} from './components/recommended-games/recommended-games.component';
+import {HttpClient} from '@angular/common/http';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent, RecommendedGamesComponent],
+  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent, RecommendedGamesComponent, NgIf],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'VsnFrontend';
+  routerLinks = ''
+
+  constructor(private router: Router) {
+      this.routerLinks = this.router.url;
+      console.log(this.routerLinks);
+  }
+
+
+  inLogin()
+  {
+    if(this.routerLinks === '/')
+      return false;
+
+    return true;
+  }
+
+
+
 
 }
