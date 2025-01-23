42bf38ac7f4669228572b8cc8ef9edbeda104837
jacopo <jacopo.cariato98@gmail.com>
2025-01-23 13:00:21 +0100

    recommended vg card v1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import {LoginPageComponent} from './components/login-page/login-page.component';
 import {SigninPageComponent} from './components/signin-page/signin-page.component';
 import {UserProfilePageComponent} from './components/user-profile-page/user-profile-page.component';
+import {RecommendedGamesComponent} from './components/recommended-games/recommended-games.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent],
+  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent, RecommendedGamesComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
---
+++ src/app/components/recommended-games/recommended-games.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recommended-games',
+  imports: [],
+  templateUrl: './recommended-games.component.html',
+  standalone: true,
+  styleUrl: './recommended-games.component.css'
+})
+export class RecommendedGamesComponent {
+
+}
