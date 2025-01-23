3fee216dfc2a3ea5e6e523f89d4d0b545bbcecab
jacopo <jacopo.cariato98@gmail.com>
2025-01-23 15:21:18 +0100

    left navbar v1: only positional text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {LoginPageComponent} from './components/login-page/login-page.component';
 import {SigninPageComponent} from './components/signin-page/signin-page.component';
 import {UserProfilePageComponent} from './components/user-profile-page/user-profile-page.component';
 import {RecommendedGamesComponent} from './components/recommended-games/recommended-games.component';
+import {LeftNavbarComponent} from './components/left-navbar/left-navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent, RecommendedGamesComponent],
+  imports: [RouterOutlet, LoginPageComponent, SigninPageComponent, UserProfilePageComponent, RecommendedGamesComponent, LeftNavbarComponent],
   templateUrl: './app.component.html',
   standalone: true,
   styleUrl: './app.component.css'
---
+++ src/app/components/left-navbar/left-navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-navbar',
+  imports: [],
+  templateUrl: './left-navbar.component.html',
+  standalone: true,
+  styleUrl: './left-navbar.component.css'
+})
+export class LeftNavbarComponent {
+
+}
