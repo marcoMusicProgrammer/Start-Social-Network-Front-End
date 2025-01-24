5bc5245fcd40f9470e7ce49e9ef3689190aeac66
jacopo <jacopo.cariato98@gmail.com>
2025-01-24 15:44:38 +0100

    videogame page and videogame card (draft 2)

--- src/app/components/videogame-card/videogame-card.component.ts
+++ src/app/components/videogame-card/videogame-card.component.ts
@@ -8,21 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './videogame-card.component.css'
 })
 export class VideogameCardComponent {
-  // Stato del cuore
-  heartSelected = false;
-
-  // Stato delle stelle
-  stars = Array(5).fill(0); // Un array di 5 elementi
-  rating = 0;
-
-  // Toggle del cuore
-  toggleHeart() {
-    this.heartSelected = !this.heartSelected;
-  }
-
-  // Imposta il livello di apprezzamento tramite le stelle
-  rate(value: number) {
-    this.rating = value;
-  }
+  // // Stato del cuore
+  // heartSelected = false;
+  //
+  // // Stato delle stelle
+  // stars = Array(5).fill(0); // Un array di 5 elementi
+  // rating = 0;
+  //
+  // // Toggle del cuore
+  // toggleHeart() {
+  //   this.heartSelected = !this.heartSelected;
+  // }
+  //
+  // // Imposta il livello di apprezzamento tramite le stelle
+  // rate(value: number) {
+  //   this.rating = value;
+  // }
 
 }
